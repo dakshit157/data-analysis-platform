ced28f155ce84a371a048540b7338d0a65901c28
Dakshit Rathod <dakshitr157@gmail.com>
2026-09-17 01:16:47 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="DataLens API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000" , "https://data-analysis-platform-lake.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
